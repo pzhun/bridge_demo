2b03116b0f7671d681de85327f3948ada089d20d
pzhun <1414807496@qq.com>
2026-02-24 17:44:31 +0800

    update

---
+++ usdt0Demo.js
@@ -0,0 +1,116 @@
+const { ethers } = require("ethers");
+const UserWallet = require("./services/userWallet");
+const config = require("./config/config");
+
+// ---------- USDT0 配置 (Unichain -> Arbitrum) ----------
+// 参考: https://docs.usdt0.to/technical-documentation/deployments
+const UNICHAIN_USDT0_TOKEN = "0x9151434b16b9763660705744891fA906F660EcC5";
+const UNICHAIN_OFT = "0xc07bE8994D035631c36fb4a89C918CeFB2f03EC3";
+// ------- 配置区 -------
+const PRIVATE_KEY = config.wallets.gasPayer.privateKey;
+
+const networks = {
+  unichain: {
+    chainId: 130,
+    rpc: "https://mainnet.unichain.org",
+  },
+  arbitrum: {
+    chainId: 42161,
+    rpc: "https://arbitrum-mainnet.infura.io/v3/f0443451e6034c60830c9ca206431876",
+  },
+};
+
+const wallet = new UserWallet(PRIVATE_KEY);
+const userAddress = wallet.address;
+const recipientAddress = wallet.address;
+
+const fromToken = {
+  chain: "unichain",
+  address: UNICHAIN_USDT0_TOKEN,
+  oftAddress: UNICHAIN_OFT,
+};
+const toToken = {
+  chain: "arbitrum",
+  address: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9", // Arbitrum USDT0
+};
+
+const bridgeName = "usdt0 (LayerZero OFT)";
+
+function getNetworkByChainName(chainName) {
+  return networks[chainName] || networks.unichain;
+}
+
+
+async function main() {
+  const requestData = {
+    user_address: wallet.address,
+    from_chain: fromToken.chain,
+    from_token_address: fromToken.address,
+    to_token_address: toToken.address,
+    to_chain: toToken.chain,
+    amount: "1",
+    bridge: bridgeName,
+  };
+  console.log(requestData);
+
+}
+
+async function finalizeTransaction(unsignedTx, network) {
+  const provider = new ethers.JsonRpcProvider(network.rpc);
+  const nonce = await provider.getTransactionCount(unsignedTx.from);
+
+  // 确保 chainId 正确设置
+  const txWithChainId = {
+    ...unsignedTx,
+    chainId: network.chainId,
+  };
+
+  const gasLimit = await provider.estimateGas(txWithChainId);
+  const gasPrice = await provider.getFeeData();
+  const maxFeePerGas = gasPrice.maxFeePerGas;
+  const maxPriorityFeePerGas = gasPrice.maxPriorityFeePerGas;
+  return {
+    ...txWithChainId,
+    type: 2, // EIP-1559
+    nonce,
+    gasLimit,
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+  };
+}
+
+// 查询是否approve
+async function approve(fromToken, contractAddress, network) {
+  const provider = new ethers.JsonRpcProvider(network.rpc);
+  const erc20Contract = new ethers.Contract(
+    fromToken.address,
+    ERC20ABI,
+    provider
+  );
+
+  const allowance = await erc20Contract.allowance(userAddress, contractAddress);
+  if (allowance <= 0) {
+    const data = erc20Contract.interface.encodeFunctionData("approve", [
+      contractAddress,
+      ethers.MaxUint256,
+    ]);
+    const tx = {
+      from: userAddress,
+      to: fromToken.address,
+      data: data,
+    };
+    const unsignedTx = await finalizeTransaction(tx, network);
+    const signedTx = await wallet.signTransaction(unsignedTx);
+    const hash = await wallet.broadcastTransaction(network.rpc, signedTx);
+    console.log(hash);
+  }
+}
+
+
+
+
+if (require.main === module) {
+  main().catch(console.error);
+}
+
+module.exports = { main };
