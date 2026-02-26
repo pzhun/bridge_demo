cca9042e922498916c09d9f1af46a67f93ba2781
pzhun <1414807496@qq.com>
2026-02-26 10:14:38 +0800

    update:

--- usdt0Demo.js
+++ usdt0Demo.js
@@ -1,16 +1,21 @@
 const { ethers } = require("ethers");
+const axios = require("axios");
 const UserWallet = require("./services/userWallet");
 const config = require("./config/config");
 const ERC20ABI = require("./config/erc20.json");
 
-
 // ------- 配置区 -------
 const PRIVATE_KEY = config.wallets.gasPayer.privateKey;
+
+const host = "https://api.fxwallet.in";
+const quoteApi = "/bridge/quote";
+const executeApi = "/bridge/execute";
+const txDetailApi = "/bridge/record";
 
 const networks = {
   unichain: {
     chainId: 130,
-    rpc: "https://unichain-mainnet.infura.io/v3/f0443451e6034c60830c9ca206431876",
+    rpc: "https://unichain-mainnet.g.alchemy.com/v2/_EsmcL9t4r5YyrlZKpb9Wx7Tj7__cY3W",
   },
   arbitrum: {
     chainId: 42161,
@@ -39,11 +44,12 @@ const toToken = {
   lzId: 30110
 };
 
-const bridgeName = "usdt0 (LayerZero OFT)";
+const bridgeName = "usdt0 bridge";
 
 function getNetworkByChainName(chainName) {
   return networks[chainName] || networks.unichain;
 }
+
 
 
 async function main() {
@@ -53,14 +59,37 @@ async function main() {
     from_token_address: fromToken.address,
     to_token_address: toToken.address,
     to_chain: toToken.chain,
-    amount: "1",
+    amount: "0.2",
     bridge: bridgeName,
   };
 
-  const network = getNetworkByChainName(requestData.from_chain);
+  // 准备跨链交易
 
-  // approve
-  await approve(fromToken, fromToken.oftAddress, network);
+  const network = getNetworkByChainName(requestData.from_chain);
+  const bridgeData = await axios.post(`${host}${quoteApi}`, requestData);
+  const bridgeTx = bridgeData.data.quotes[0].unsigned_tx[0];
+  await approve(fromToken, bridgeTx.to, network);
+
+  // 发送跨链交易
+  const finalizedTx = await finalizeTransaction(bridgeTx, network);
+  console.log(finalizedTx);
+  const signedTx = await wallet.signTransaction(finalizedTx);
+  const executeData = {
+    bridge: bridgeName,
+    user_address: userAddress,
+    from_chain: requestData.from_chain,
+    to_chain: requestData.to_chain,
+    from_token_address: requestData.from_token_address,
+    to_token_address: requestData.to_token_address,
+    extra_data: bridgeData.data.quotes[0].extra_data,
+    signed_tx: signedTx,
+    type: "execute",
+  };
+  console.log(executeData);
+  const executeTx = await axios.post(`${host}${executeApi}`, executeData);
+  console.log(executeTx);
+
+
 
 }
 
